129fa743d5e54830a637e1254274c5c871fb25db
Minh NQ <uocmoxavoi1102@gmail.com>
2023-05-30 16:05:04 +0700

    Init commit

---
+++ js/main.js
@@ -0,0 +1,1 @@
+console.log('kha` kha`')
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 3002,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        postDetail: resolve(__dirname, 'post-detail.html'),
+        addEditPost: resolve(__dirname, 'add-edit-post.html'),
+      },
+    },
+  },
+})
